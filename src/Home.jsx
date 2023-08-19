57fb509cacc9f41d8dd542d130b99855abf5dc1d
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-20 00:03:24 +0530

    redux

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import {useDispatch} from "react-redux";
+import {useDispatch,useSelector} from "react-redux";
 
 const Home = () => {
-const value=20;
+
 const dispatch=useDispatch();
 
+const {c}=useSelector(state=>state.custom);
 const addBtn=()=>{
 dispatch({
     type:"increment",
@@ -18,7 +19,7 @@ dispatch({
 
   return (
     <div>
-      <h2>{value}</h2>
+      <h2>{c}</h2>
       <button onClick={addBtn}>Increment</button>
       <button onClick={subBtn}>Decrement</button>
     </div>
--- src/reducer.js
+++ src/reducer.js
@@ -7,10 +7,6 @@ export const customReducer=createReducer(initialState,{
     increment:(state,action)=>{
         state.c +=1;
     },
-    incrementByValue:(state,action)=>{
-        state.c+=1;
-        state.c+=action.payload;
-    },
     decrement:(state,action)=>{
         state.c-=1;
     }
