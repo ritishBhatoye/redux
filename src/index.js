3400ce5c62771e6f0587bc2e5ba1c4c86daa0e20
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-19 23:53:35 +0530

    redux

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import store from './store'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={}>
+    <Provider >
     <App />
 </Provider>
   </React.StrictMode>
